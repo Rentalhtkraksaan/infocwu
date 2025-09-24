920ee6156b713ee048120b4daf6aa270963a57b2
Rentalhtkraksaan <rentalhtkraksaan@gmail.com>
2025-09-24 08:50:14 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function openModal(title, desc) {
-  document.getElementById('modalTitle').innerText = title;
-  document.getElementById('modalDesc').innerText = desc;
-  document.getElementById('modal').classList.remove('hidden');
-}
-
-function closeModal() {
-  document.getElementById('modal').classList.add('hidden');
-}
